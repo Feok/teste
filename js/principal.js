055b842153e6efbaf484db76c940b5bcb6a7c4b1
Felipe de Queiroz <fe.dqueiroz@gmail.com>
2018-10-01 11:22:51 -0300

    aula 03 - parte 2

--- js/principal.js
+++ js/principal.js
@@ -6,7 +6,7 @@ var pacientes = document.querySelectorAll(".paciente");
 for(var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
     
-    var tdPeso = document.querySelector(".info-peso");
+    var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
 
     var tdAltura = paciente.querySelector(".info-altura");
@@ -21,12 +21,14 @@ for(var i = 0; i < pacientes.length; i++) {
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
     }
 
     if(altura < 0 || altura > 3.0) {
         console.log("Altura inválida");
         alturaEhValida = false;
         tdImc.textContent = "Altura Inválido";
+        paciente.style.backgroundColor = "lightcoral";
     }
 
     if(alturaEhValida && pesoEhValido) {
