409aae5caaa0f696d14f82f1d9ad32caf6469446
Felipe de Queiroz <fe.dqueiroz@gmail.com>
2018-10-02 15:13:03 -0300

    aula 06 - parte 03

--- js/form.js
+++ js/form.js
@@ -10,11 +10,10 @@ botaoAdicionar.addEventListener("click", function(event){
 
     var pacienteTr = montaTr(paciente);
     
-    var erro = validaPaciente(paciente);
-    
-    if(erro.length > 0) {
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erro;
+    var erros = validaPaciente(paciente);
+    console.log(erros);
+    if(erros.length > 0) {
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -24,7 +23,20 @@ botaoAdicionar.addEventListener("click", function(event){
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form){
 
@@ -35,7 +47,6 @@ function obtemPacienteDoFormulario(form){
         gordura: form.gordura.value,
         imc: calculaImc(form.peso.value, form.altura.value)
     }
-
     return paciente;
 }
 
@@ -64,13 +75,20 @@ function validaPaciente(paciente) {
     
     var erros = [];
     
-    if(!validaPeso(paciente.peso)) {
-        erros.push("Peso é inválido!");
+    if(paciente.peso.length == 0 || !validaPeso(paciente.peso)) {
+        erros.push("Peso inválido!");
     }
 
-    if(!validaAltura(paciente.altura)) {
-        erros.push("Altura é inválida!");
+    if(paciente.altura.length == 0 || !validaAltura(paciente.altura)) {
+        erros.push("Altura inválida!");
+    }
+    if(paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco.");
     }
 
+    if(paciente.gordura.length == 0) {
+        erros.push("O valor da gordura corporal deve ser informado.");
+    }
     return erros;
 }
+
