252afee2dabf92bb5fc4c5aedb0a65c8efe9fa8e
Felipe de Queiroz <fe.dqueiroz@gmail.com>
2018-10-02 16:06:58 -0300

    aula 07 - parte 03

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,13 +3,12 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-    event.target.parentNode.remove();
+    
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 });
 
-// pacientes.forEach(function(paciente) {
-//     paciente.addEventListener("dblclick", function(){
-//         console.log("fui clicado com um duplo clique.");
-//         this.remove();
-//     });
-// });
+
 
