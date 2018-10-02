c06d5dcc7815cc449759677235e854c6226c3c79
Felipe de Queiroz <fe.dqueiroz@gmail.com>
2018-10-02 14:02:22 -0300

    aula 06 - parte 01

--- js/form.js
+++ js/form.js
@@ -10,6 +10,11 @@ botaoAdicionar.addEventListener("click", function(event){
 
     var pacienteTr = montaTr(paciente);
     
+    if(!validaPaciente(paciente)) {
+        console.log("Paciente inválido.");
+        return;
+    }
+
     // adiciona o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
@@ -50,4 +55,12 @@ function montaTd(dado, classe) {
     td.textContent = dado;
     td.classList.add(classe);
     return td;
+}
+
+function validaPaciente(paciente) {
+    if(validaPeso(paciente.peso)) {
+        return true;
+    } else {
+        return false;
+    }
 }
